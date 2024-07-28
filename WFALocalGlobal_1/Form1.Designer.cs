2500dc172df3ac2b28f145a473464abeb1c367f9
Ahmet Terzioğlu <Yevmen63@gmail.com>
2024-07-28 13:41:07 +0300

    Add project files.

---
+++ WFALocalGlobal_1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WFALocalGlobal_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDeneme = new System.Windows.Forms.Button();
+            this.btnAzalt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDeneme
+            // 
+            this.btnDeneme.Location = new System.Drawing.Point(466, 86);
+            this.btnDeneme.Name = "btnDeneme";
+            this.btnDeneme.Size = new System.Drawing.Size(75, 23);
+            this.btnDeneme.TabIndex = 0;
+            this.btnDeneme.Text = "Arttır";
+            this.btnDeneme.UseVisualStyleBackColor = true;
+            this.btnDeneme.Click += new System.EventHandler(this.btnDeneme_Click);
+            // 
+            // btnAzalt
+            // 
+            this.btnAzalt.Location = new System.Drawing.Point(471, 158);
+            this.btnAzalt.Name = "btnAzalt";
+            this.btnAzalt.Size = new System.Drawing.Size(75, 23);
+            this.btnAzalt.TabIndex = 1;
+            this.btnAzalt.Text = "Azalt";
+            this.btnAzalt.UseVisualStyleBackColor = true;
+            this.btnAzalt.Click += new System.EventHandler(this.btnAzalt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAzalt);
+            this.Controls.Add(this.btnDeneme);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDeneme;
+        private System.Windows.Forms.Button btnAzalt;
+    }
+}
+
---
+++ WFALocalGlobal_1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFALocalGlobal_1
+{
+
+
+    public partial class Form1 : Form
+    {
+
+
+        #region LocalAreaNotlari
+
+        //Local alan  (Local Area) su anda bu yorum satırlarını yazdıgımız yerin (Form1 yasam alanının) icerisinde yer alan ekstra scope'tur(Scope,block, yaşam alanı eş anlamlı kullanılır...)
+
+        //Local alanın bir dısında kalan alan da global alandır...
+
+
+        //Siz kontrollerinize sadece ve sadece local alanlardan ulasabilirsiniz... Local alan dısında karsınıza cıkan turkuaz renkli kontrol ismi sakın kafanızı karıstırmasın...Onlar sistemde gömülü olan orijinal yapılardır...
+
+
+        //Her local alanınız bir event degildir...
+
+        //Local alan kendi dısarısından asla görülmez...Local alanda olan her şey sadece local alanda kalır...Dolayısıyla bir local alanda olusturulan degişken ismi baska bir local alanda da olusturulabilir... 
+
+        //Local alanları kendi iclerini göstermeyen ama bir dıslarını görebilen alanlar olarak düsünün...
+
+        //Local alan istisnasız her zaman önceligi kendisine verir...Global alanda olusturulmus bir degişken ile aynı isimde bir degişken local alanda tanımlanıyorsa local alan kendi degişkenini tanır... Local alanın icerisinde tanımlanan kendi degişkeninin yanında global alandaki aynı isimde olan degişkene de ulasmak istiyorsanız this keyword'unu kullanmanız gerekir...
+
+
+
+
+        #endregion
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+
+        int a = 1;
+
+        
+
+       
+
+        
+        
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDeneme_Click(object sender, EventArgs e)
+        {
+            //Garbage Collector
+
+
+            
+
+            int a = 2; //normalde burada yine a isimli degişken tanımlandıgı icin artık bu local alan normal şartlar altında global'deki a'yı göremez..
+
+            //this.a; => böyle diyerek global alandaki a'ya ulasabilirsiniz...
+
+
+            a++;
+            MessageBox.Show(a++.ToString()); //cıktı 3
+            MessageBox.Show(a.ToString()); //cıktı 4
+
+        }
+
+        private void btnAzalt_Click(object sender, EventArgs e)
+        {
+            a--;
+            MessageBox.Show(a.ToString());
+        }
+    }
+}
